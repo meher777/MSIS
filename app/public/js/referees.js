6e3579271ad14dfd43b2fb3ee8cdba17f85ffff1
carlschh <carlschh@iu.edu>
2021-10-30 14:35:37 -0400

    referee js

---
+++ app/public/js/referees.js
@@ -0,0 +1,126 @@
+const RefereeApp = {
+  data() {
+    return {
+      referees: [],
+      refereeForm: {},
+      selectedReferee: null
+    }
+  },
+  computed: {},
+  methods: {
+      fetchRefereeData(r) {
+        fetch('/api/referee/index.php')
+        .then( response => response.json() )
+          .then( (responseJson) => {
+              console.log(responseJson);
+              this.referees = responseJson;
+          })
+          .catch( (err) => {
+              console.error(err);
+          })
+      },
+      selectReferee(r) {
+        if (r == this.selectedReferee) {
+            return;
+        }
+        this.selectedReferee = r;
+        this.referees = [];
+        this.fetchRefereeData(this.selectedReferee);
+    },
+      postNewReferee(evt) {     
+        console.log("Creating:", this.refereeForm);
+
+        fetch('api/referee/create.php', {
+            method:'POST',
+            body: JSON.stringify(this.refereeForm),
+            headers: {
+              "Content-Type": "application/json; charset=utf-8"
+            }
+          })
+          .then( response => response.json() )
+          .then( json => {
+            console.log("Returned from post:", json);
+            // TODO: test a result was returned!
+            this.referees = json;
+            
+            // reset the form
+            this.refereeForm = {};
+          });
+      },
+      postReferee(evt){
+
+        if(this.selectedReferee){
+          this.postEditReferee(evt);
+        }
+        else{
+          this.postNewReferee(evt);
+        }
+      },
+      postEditReferee(evt){
+        this.refereeForm.id = this.selectedReferee.id;
+        this.refereeForm.id = this.selectedReferee.id;        
+        console.log("Editing:", this.refereeForm);
+        // alert("Posting!");
+
+        fetch('api/referee/update.php', {
+            method:'POST',
+            body: JSON.stringify(this.refereeForm),
+            headers: {
+              "Content-Type": "application/json; charset=utf-8"
+            }
+          })
+          .then( response => response.json() )
+          .then( json => {
+            console.log("Returned from post:", json);
+            // TODO: test a result was returned!
+            this.referees = json;
+            
+            // reset the form
+            this.handleResetEdit = {};
+          })
+          .catch( err => {
+            alert("Something went wrong.");
+          });
+      },
+      postDeleteReferee(o) {  
+        if ( !confirm("Are you sure you want to delete the Referee?") ) {
+            return;
+        }  
+        
+        console.log("Delete!", o);
+
+        fetch('api/referee/delete.php', {
+            method:'POST',
+            body: JSON.stringify(o),
+            headers: {
+              "Content-Type": "application/json; charset=utf-8"
+            }
+          })
+          .then( response => response.json() )
+          .then( json => {
+            console.log("Returned from post:", json);
+            // TODO: test a result was returned!
+            this.referees = json;
+            
+            // reset the form
+            this.handleResetEdit();
+          });
+      },
+      handleEditReferee(referee) {
+        this.selectedReferee = referee;
+        this.refereeForm = Object.assign({}, this.selectedReferee);
+      },
+      handleResetEdit() {
+          this.selectedReferee = null;
+          this.refereeForm = {};
+      }
+  },
+  created() {
+      this.fetchRefereeData();
+  }
+
+}
+
+Vue.createApp(refereeApp).mount('#refereeTable');
+
+// Resources : https://stackoverflow.com/questions/62975890/how-to-reload-refresh-a-webpage-in-vue-js-after-a-certain-event-or-a-button-is-c
